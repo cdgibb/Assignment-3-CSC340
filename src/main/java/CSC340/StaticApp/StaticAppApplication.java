fef79b3f7452222f24423eda358cb471e6989757
cdgibb <cdgibb2015@gmail.com>
2024-12-14 19:59:23 -0500

    Added files

---
+++ src/main/java/CSC340/StaticApp/StaticAppApplication.java
@@ -0,0 +1,13 @@
+package CSC340.StaticApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CSC340/StaticApp/StaticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package CSC340.StaticApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
